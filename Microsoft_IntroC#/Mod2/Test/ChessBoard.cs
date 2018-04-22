10040b3f72eb691f1459f66ccd9a1d72087fc20a
Jim McGee <jmcgee86@gmail.com>
2018-04-22 10:53:04 -0400

    completed microsoft intro to c#

---
+++ Microsoft_IntroC#/Mod2/Test/ChessBoard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x <= 8)
+            {
+                if (x % 2 !=0){
+                    Console.WriteLine("XOXOXOXO");
+                    x++;
+                }
+                else{
+                    Console.WriteLine("OXOXOXOX");
+                    x++;
+                }
+                
+            }        
+        }
+    }
+}
---
+++ Microsoft_IntroC#/Mod3/Lab1/Mod3_Lab1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Sum(20,40);
+            Console.WriteLine($"Calling Sum() with two arguments, result is: {result}");
+
+            int result3 = Sum(10, 50, 80);
+            Console.WriteLine($"Calling Sum() with three arguments, result is: {result3}");
+
+            double dblResult = Sum(20.5, 30.6);
+            Console.WriteLine($"Calling Sum() that takes doubles, result is: {dblResult}");
+        }
+
+        // Sum() method that takes two integer arguments and sums them
+        // The method returns no value which is why we use void
+        // We also need to use static in the method signature because Main is static
+        // and you cannot call a non-static method from a static method
+        static int Sum(int first, int second)
+        {
+            int sum = first + second;
+            return sum;
+        }
+
+        static int Sum(int first, int second, int third)
+        {
+            int sum = first + second + third;
+            return sum;
+        }
+
+        static double Sum(double first, double second)
+        {
+            double result = first + second;
+            return result;
+        }
+    
+    }
+}
