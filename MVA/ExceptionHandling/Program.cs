cf1b8906ae8921fa987383264372de8cae027707
Jim McGee <jmcgee86@gmail.com>
2018-09-15 11:26:28 -0400

    Exception Handling

---
+++ MVA/ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"/Users/jmcgee/Documents/cSharp/cSharpPractice/MVA/ExceptionHandling/Example.txt");
+                Console.WriteLine(content);
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("there was a problem!");
+                Console.WriteLine("Make sure the name of the file is correct");              
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("there was a problem!");
+                Console.WriteLine("Make sure the directory exists");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("there was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //code to finalize, ex:
+                    //setting objects to null
+                    //closing database connection
+                Console.WriteLine("closing application now...");
+            }
+          
+        }
+    }
+}
